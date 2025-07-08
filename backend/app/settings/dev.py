a6ee4cc7808a27f05a0a5a41d9073da43a3e28da
programmerrm <programmerwebrm@gmail.com>
2025-07-08 21:15:51 +0600

    add

--- backend/app/settings/dev.py
+++ backend/app/settings/dev.py
@@ -13,6 +13,8 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost:3000', 
     '127.0.0.1:8000',
+    'api.bjollys.net',
+    'bjollys.net',
     'de1f-118-179-170-140.ngrok-free.app',
 ]
 
@@ -35,12 +37,18 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:8000',
     'http://localhost:5173',
+    'https://api.bjollys.net',
+    'https://www.bjollys.net',
+    'https://bjollys.net',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://localhost:5173',
+    'https://api.bjollys.net',
+    'https://www.bjollys.net',
+    'https://bjollys.net',
 ]
 
 REST_FRAMEWORK['DEFAULT_SCHEMA_CLASS'] = 'drf_spectacular.openapi.AutoSchema'
