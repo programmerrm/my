3c3edcca8a465d566bee14fc9e120daeb75e1a2f
programmerrm <programmerwebrm@gmail.com>
2025-07-12 19:00:49 +0600

    docker compose

--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -1,10 +1,10 @@
 import type { MetaFunction } from "@remix-run/node";
 import { About } from "~/components/about/about";
 import { Banner } from "~/components/banner/banner";
-import { Footer } from "~/components/footer/footer";
 import { Services } from "~/components/services/services";
 import { Team } from "~/components/team/team";
 import { Why_Us } from "~/components/why_us/why_us";
+import { SERVER_URL } from "~/utils/api";
 
 export const meta: MetaFunction = () => {
   return [
@@ -14,6 +14,7 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  console.log('SERVER URL', SERVER_URL);
   return (
     <section className="relative top-0 left-0 right-0 w-full">
       <section className="relative top-0 left-0 right-0 w-full">
