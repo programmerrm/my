80b9ce30f605393a1ed791f2cfb2fba66c9439a5
programmerrm <programmerwebrm@gmail.com>
2025-07-12 19:31:59 +0600

    new add

--- backend/app/settings/prod.py
+++ backend/app/settings/prod.py
@@ -34,23 +34,3 @@ CORS_ALLOWED_ORIGINS = [
     "https://bjollys.net",
     "https://api.bjollys.net",
 ]
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/app/logs/django.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
-
